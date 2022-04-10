304484b268e3dffbdd03b4fd6b7711a6018d5ac7
Joan Cueva Moreno <jcueva@inergybcn.com>
2022-04-10 22:34:23 +0200

    Accedir als links de les categories de comics i guardar el contingut

--- src/scraper.py
+++ src/scraper.py
@@ -4,16 +4,41 @@ from bs4 import BeautifulSoup
 
 class Scraper:
 
+    const_comics_category = "/comics/"
+
     def __init__(self):
         print("Scraper inicialitzat")
 
-    def save_html(self, html, path):
-        with open(path, 'wb') as f:
+    def request_html(self, url):
+        return requests.get(url)
+
+    def save_html(self, html, file_name):
+        with open(f"data/{file_name}", 'wb') as f:
             f.write(html)
 
-    def open_html(self, path):
-        with open(path, 'rb') as f:
+    def open_html(self, file_name):
+        with open(f"data/{file_name}", 'rb') as f:
             return f.read()
+
+    def get_content_links(self, html):
+        soup = BeautifulSoup(html, 'html.parser')
+
+        catalog = soup.select("ul li")
+        categories = [category.text.strip() for category in catalog]
+
+        a_tags = [category.select_one('a') for category in catalog]
+        comics_links = [a_tag['href'] for a_tag in a_tags if a_tag is not None and self.const_comics_category in a_tag['href']]
+
+        comics_links_names = self.get_content_links_names(comics_links)
+
+        requests = [self.request_html(link) for link in comics_links]
+        for request, name in zip(requests, comics_links_names):
+            self.save_html(request.content, name)
+
+        return comics_links_names
+
+    def get_content_links_names(self, links):
+        return [link.split(self.const_comics_category)[1] for link in links]
 
 
     def scrape(self):
@@ -26,27 +51,13 @@ class Scraper:
         # Accedim a la url i guardem el contingut en local per no haver de tornar a accedir
 
         url = 'https://www.ecccomics.com/'
+        # r = self.request_html(url)
 
-        #r = requests.get(url)
-        #print(r.content[:100])
-
-        #self.save_html(r.content, 'data/ecccomics')
-        html = self.open_html('data/ecccomics')
+        # self.save_html(r.content, 'data/ecccomics')
+        html = self.open_html('ecccomics')
 
         # BeautifulSoup
 
-        soup = BeautifulSoup(html, 'html.parser')
-
-        catalog = soup.select("ul li")
-        #print(catalog)
-
-        categories = [category.text.strip() for category in catalog]
-        #print(categories)
-
-        a_tags = [category.select_one('a') for category in catalog]
-        links = [a_tag['href'] if a_tag != None else "" for a_tag in a_tags]
-
-        print(links)
-
+        comics_links = self.get_content_links(html)
 
 
